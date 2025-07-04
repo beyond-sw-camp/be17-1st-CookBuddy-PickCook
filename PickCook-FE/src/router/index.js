c99b51d4243f6809a599e3995985ce6c939e8525
John Heo <jungwoocity0327@gmail.com>
2025-07-04 20:15:33 +0900

    [feat] 쇼핑페이지/ 쇼핑 디데일 / 아이템 카드  예시 추가

--- PickCook-FE/src/router/index.js
+++ PickCook-FE/src/router/index.js
@@ -3,6 +3,8 @@ import RecipeDetail from '@/views/RecipeDetail.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import Community from '@/views/Community.vue'
 import CommunityDetail from '@/views/CommunityDetail.vue'
+import Shopping from '@/views/Shopping.vue'
+import Shopping_detail from '@/views/Shopping_detail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,7 +28,17 @@ const router = createRouter({
       path: '/community/detail',
       name: 'communityDetail',
       component: CommunityDetail
-    }
+    },
+    {
+      path: '/shopping',
+      name: 'shopping',
+      component: Shopping,
+    },
+    {
+      path: '/shopping/detail',
+      name: 'shoppingdetail',
+      component: Shopping_detail,
+    },
   ],
 })
 
