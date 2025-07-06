1990eb8863ad6a8cd7bf2a3b0dd7f34418326b3c
thay123028 <thay123028@naver.com>
2025-07-06 17:52:12 +0900

    [feat] 마이페이지 기본 틀 구현

--- PickCook-FE/src/views/Mypage.vue
+++ PickCook-FE/src/views/Mypage.vue
@@ -1,11 +1,28 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-<div>마이페이지 틀</div>
+  <div class="mypage-body">
+    <div class="mypage-body-child">
+      <!-- 서브바 들어갈 부분 -->
+
+      <!-- 내용 부분 -->
+      <div class="mypage-main">
+        <!-- 내용 인디케이터 -->
+        <div class="mypage-main-header">
+          <div class="mypage-indicator-container">
+            <div id="none-cursor" class="mypage-indicator-items">
+              <!-- 하위 라우터 제목 들어갈 부분 -->
+            </div>
+          </div>
+        </div>
+
+        <!-- 내용 -->
+        <div class="mypage-main-contents-body">
+          <!-- 하위 라우터 내용 들어갈 부분 -->
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
